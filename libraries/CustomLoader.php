6b48ca78a5079101602ed0277b8349209ef1e3ce
prov <provinite@gmail.com>
2015-10-18 05:33:54 -0500

    Adds randomness library and proposed algorithm. Fixes #6

--- libraries/CustomLoader.php
+++ libraries/CustomLoader.php
@@ -5,6 +5,7 @@ require_once("entities/Prize.php");
 require_once("entities/User.php");
 require_once("entities/WinEvent.php");
 require_once("entities/TrickOrTreatEvent.php");
+require_once("random_compat/random.php");
 class CustomLoader {
 
 }
