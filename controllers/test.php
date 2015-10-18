f5e3604e626aa96e87e2848ef32731fcd055bc7c
prov <provinite@gmail.com>
2015-10-18 05:29:44 -0500

    Adds randomness lib. Proposed algorithm for prize winning.

--- controllers/test.php
+++ controllers/test.php
@@ -31,6 +31,66 @@ class Test extends CI_Controller {
 
     }
 
+    public function getRandomPrize() {
+        $this->output->enable_profiler();
+        $prizes = $this->prizedao->getWithStockGreaterThan(0);
+        echo "<pre>";
+        $stats = array(0,0,0,0,0,0,0,0);
+        $iterations = 12000;
+        $winrate = 60;
+        $prizeRanges = array();
+        for ($k = 0; $k < $iterations; $k++) {
+            $winner = (random_int(0, 9999) <= $winrate);
+            if (!$winner) {
+                $stats[0]++;
+                continue;
+            }
+            $prizeRanges = array();
+            $start = 0;
+            $end = 0;
+            foreach ($prizes as $prize) {
+                $range = $prize->getStock() * $prize->getWeight();
+                $end = floor($start + 100*$range) - 1;
+                $prizeRanges[] = array(
+                    'min' => $start,
+                    'max' => $end,
+                    'prize' => $prize
+                );
+                $start = $end + 1;
+            }
+            if ($end <= 0) { $stats[0]++; continue; }
+            $selection = random_int(0, $end);
+            foreach ($prizeRanges as $range) {
+                if ($selection >= $range['min'] && $selection <= $range['max']) {
+                    $prize = $range['prize'];
+                    $prize->setStock($prize->getStock() - 1);
+                    $stats[$range['prize']->getId()]++;
+
+                    break;
+                }
+            }
+        }
+        $totalwinners = 0;
+        foreach ($stats as $key => $stat) {
+            $prizename = "";
+            if ($stat == 0) {
+                continue;
+            }
+            if ($key > 0) {
+                $totalwinners += $stat;
+            }
+            if ($key > 0 && array_key_exists($key-1, $prizes)) {
+                $prizename = $prizes[$key-1]->getName();
+            }
+            $percent = $stat / $iterations;
+            $percent = $percent * 100;
+            echo "$key: $stat $prizename ($percent%)\n";
+        }
+        echo "Win Rate: " . $winrate / 10000 * 100 . "%\n";
+        echo "Total Winners: $totalwinners (" . ($totalwinners - 81) . ")";
+        echo "\n";
+    }
+
     public function patchTest() {
         $u1 = new User();
         $u2 = new User();
